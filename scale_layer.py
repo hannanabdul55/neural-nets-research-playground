b52cc3f1a57b82c5223a24180757ecad48aabe0f
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2021-02-21 20:33:49 -0500

    Fix norm calc 2

--- scale_layer.py
+++ scale_layer.py
@@ -27,7 +27,7 @@ class ScaleLayer(nn.Module):
         max_val = torch.max(torch.abs(input.view(input.shape[0], -1)), dim=1)[0]
         print(f"Max value shape: {max_val.shape}")
         print(f"Input shape: {input.shape}")
-        res = (input/torch.max(torch.abs(input.view(input.shape[0], -1)), dim=1)[0].expand(input.shape[0], (1,)*input.dim()-1))
+        res = (input/max_val.expand(input.shape[0], *((1,)*(input.dim()-1))))
         if self.writer is not None:
             self.writer.add_scalar("Loss/Alpha", self.alpha.data)
         #             print("alpha:" + str(self.alpha))
